9460bbb530f23c32d7cd3ebdc33ed6e20bc4927c
vpgits <vp.mb@protonmail.com>
2023-11-14 11:39:23 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from io import BytesIO
+
+from PyPDF2 import PdfReader
+from fastapi import FastAPI, File
 
 app = FastAPI()
 
@@ -11,3 +14,16 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/parse")
+#async function which will extract a pdf file from the post request
+async def parse_pdf(file: bytes = File(...)):
+    try:
+        reader = PdfReader(BytesIO(file))
+        metadata = reader.metadata
+        return {"message": f"Hello {metadata}"}
+    except Exception as e:
+        return {"message": f"Hello {e}"}
+
+
