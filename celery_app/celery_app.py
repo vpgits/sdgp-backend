011e97c89cc650f4b881873a70826073236dfdcd
vpgits <vp.mb@protonmail.com>
2024-02-04 13:47:39 +0530

    Modified the docker compose configuration
    Update Celery broker URL and add dotenv support

--- celery_app/celery_app.py
+++ celery_app/celery_app.py
@@ -1,9 +1,15 @@
 from celery import Celery
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Celery(
     "worker",
     # broker="pyamqp://guest@localhost//",  # Use the service name as defined in docker-compose.yml
-    broker="pyamqp://sdgp_admin:59h@WeMyBtfV$YD@rabbitmq//",  # Use the service name as defined in docker-compose.yml
+    broker=os.getenv(
+        "BROKER_URL"
+    ),  # Use the service name as defined in docker-compose.yml
     backend="rpc://",
     include=[
         "celery_tasks.preprocess",
