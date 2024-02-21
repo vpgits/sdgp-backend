852a17d8816d86d66f470574fb0671332764047e
vpgits <vp.mb@protonmail.com>
2024-02-21 14:04:55 +0530

    updated dockerfile
    Update Dockerfile to copy celery_workers directory
    
    minor changes to celery config

--- celery_app/celery_app.py
+++ celery_app/celery_app.py
@@ -14,10 +14,10 @@ app = Celery(
         "BACKEND_URL"
     ),  # Use the service name as defined in docker-compose.yml
     include=[
-        "celery_tasks.preprocess",
-        "celery_tasks.generate_mcq",
-        "celery_tasks.tick_tock",
-        "celery_tasks.quiz",
+        "celery_workers.celery_tasks.preprocess",
+        "celery_workers.celery_tasks.generate_mcq",
+        "celery_workers.celery_tasks.tick_tock",
+        "celery_workers.celery_tasks.quiz",
     ],
 )
 app.conf.broker_connection_retry_on_startup = True
