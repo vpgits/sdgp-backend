a7f14c422a882ed10dcdc3f5beac91e5681723b3
Avisha Dilhara <avishadilhara.edu@gmail.com>
2024-03-07 13:54:12 +0530

    Remove unnecessary test files

--- tests/api_test/parse_test.py
+++ tests/api_test/parse_test.py
@@ -4,29 +4,29 @@ from celery_workers.src.api.parse import read_docx
 from celery_workers.src.api.parse import read_pptx
 
 def test_read_pdf():
-    result = read_pdf('../tests/test.pdf')
+    result = read_pdf('../tests/api_test/test.pdf')
     print(result)
     assert [' hi this a Pytest for PDF document to text! '] == result
 
 def test_read_pdf_fail():
     with pytest.raises(Exception):
-        read_pdf('../tests/test.txt')
+        read_pdf('../tests/api_test/test.txt')
 
 def test_read_docx():
-    result = read_docx('../tests/test.docx')
+    result = read_docx('../tests/api_test/test.docx')
     print(result)
     assert['Hi, this is a Pytest for DOCX document to text!', '', '']== result
 
 def test_read_docx_fail():
     with pytest.raises(Exception):
-        read_docx('../tests/test.txt')
+        read_docx('../tests/api_test/test.txt')
 
 def test_read_pptx():
-    result = read_pptx('../tests/test.pptx')
+    result = read_pptx('../tests/api_test/test.pptx')
     print(result)
     assert['hi this a Pytest for PPTX document to text!', '']==result
 
 def test_read_pptx_fail():
     with pytest.raises(Exception):
-        read_pptx('../tests/test.txt')
+        read_pptx('../tests/api_test/test.txt')
         
