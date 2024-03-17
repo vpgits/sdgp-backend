4a0907ab89502be5503860eb33c95c9f7cdd1ef3
Genodqwe <93708703+Genodqwe@users.noreply.github.com>
2024-03-17 11:31:49 +0530

    Added documentation for fastapi app

--- fastapi_app/src/main.py
+++ fastapi_app/src/main.py
@@ -1,101 +1,18 @@
-from celery.result import AsyncResult
-from fastapi import FastAPI, HTTPException, Depends, Request
-from celery_app.celery_app import app as celery_app
-from models.main import PreprocessBody, GenerateMCQBody, QuizBody
-
-app = FastAPI()
+from email.policy import default
+from pydantic import BaseModel
+from typing import List
 
 
-def extract_header_auth_tokens(request: Request) -> tuple[str, str]:
-    access_token = request.headers.get("authorization")
-    refresh_token = request.headers.get("refresh-token")
-    if not access_token:
-        raise HTTPException(status_code=401, detail="Authorization token is missing")
-    return access_token, refresh_token
+class PreprocessBody(BaseModel):
+    path: str
+    document_id: str
 
 
-@app.post("/preprocess/")
-async def preprocess(
-    request: PreprocessBody,
-    tokens: tuple[str, str] = Depends(extract_header_auth_tokens),
-):
-    task = celery_app.send_task(
-        "preprocess", args=[request.path, request.document_id, *tokens]
-    )
-
-    return {
-        "task_id": task.id,
-        "message": "Task has been sent to the background worker",
-    }
+class GenerateMCQBody(BaseModel):
+    key_point_id: int
+    question_id: str
 
 
-@app.post("/generate_mcq/")
-async def generate_mcq(
-    request: GenerateMCQBody,
-    tokens: tuple[str, str] = Depends(extract_header_auth_tokens),
-):
-    task = celery_app.send_task(
-        "generate_mcq", args=[request.question_id, request.key_point_id, *tokens]
-    )
-
-    return {
-        "task_id": task.id,
-        "message": "Task has been sent to the background worker",
-    }
-
-
-@app.get("/ticktock/")
-async def tick_tock():
-    task = celery_app.send_task("ticktock")
-
-    return {
-        "task_id": task.id,
-        "message": "Task has been sent to the background worker",
-    }
-
-
-@app.post("/quiz/")
-async def quiz(
-    request: QuizBody, tokens: tuple[str, str] = Depends(extract_header_auth_tokens)
-):
-    task = celery_app.send_task(
-        "quiz", args=[request.quiz_id, request.default_model, *tokens]
-    )
-    return {
-        "task_id": task.id,
-        "message": "Task has been sent to the background worker",
-    }
-
-
-@app.post("/rapid-quiz/")
-async def rapid_quiz(
-    request: QuizBody, tokens: tuple[str, str] = Depends(extract_header_auth_tokens)
-):
-    task = celery_app.send_task(
-        "rapid-quiz", args=[request.quiz_id, request.default_model, *tokens]
-    )
-    return {
-        "task_id": task.id,
-        "message": "Task has been sent to the background worker",
-    }
-
-
-@app.get("/{worker_name}/{task_id}")
-async def check_task_status(worker_name: str, task_id: str):
-    """
-    Check the status of a task.
-
-    Args:
-        worker_name (str): The name of the worker function.
-        task_id (str): The ID of the task.
-
-    Returns:
-        dict: A dictionary containing the task ID, task status, and task result.
-    """
-    worker_functions = ["preprocess", "generate_mcq", "ticktock", "quiz", "rapid-quiz"]
-
-    if worker_name not in worker_functions:
-        raise HTTPException(status_code=404, detail="Worker not found")
-
-    task = AsyncResult(task_id, app=celery_app)
-    return {"task_id": task_id, "task_status": task.state, "task_result": task.info}
+class QuizBody(BaseModel):
+    quiz_id: str
+    default_model:bool
